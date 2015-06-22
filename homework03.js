0e02f5f403d9acc9f1bf023ac6852badccae329e
zobiejrz <ben@zobie.com>
2015-06-22 10:22:05 -0400

    homework03.js outline
    The basic outline for homework03 that includes everything except the
    propmt.

---
+++ homework03.js
@@ -0,0 +1,24 @@
+//initialize variables
+var X = 0;
+var Y =0;
+var A = 0;
+var B = 0;
+var C = 0;
+var D = 0;
+var E = 0;
+var F = 0;
+//Introduction / explanation
+console.log("I am a calculation device for solving equations similar to: ax + by = c. All you have to do is give me six numbers for to equations were X and Y equal the same thing.");
+//prompt for numbers
+//save numbers to variables
+
+//calculate answer
+X = ((C * E) - (F * B)) / ((A * E) - (D * B));
+Y = ((A * F) - (D * F)) / ((A * C) - (D * B));
+//display correct answer
+console.log("Using the number you gave us, the equation is:");
+console.log(A + "x + df" + B + "y = " + C);
+console.log(" and, ");
+console.log(D + "x + " + E + "y = " + F);
+console.log("In this equatin, 'X' is equal to " + X + ", and 'Y' is equal to " + Y + ".");
+//All done!
