ea10264ea3cda7b6567e081c965dce640b99d3e1
zobiejrz <ben@zobie.com>
2015-06-23 08:32:50 -0400

    homework03.js - Everything except spaces
    All of the basic code is in, I just need to make the code split the
    prompt every time a space is detected and turn the smaller string into
    an integer

--- homework03.js
+++ homework03.js
@@ -1,18 +1,51 @@
+function onErr(err)
+{
+	console.log(err);
+	return 1;
+}
 //initialize variables
+var split = "0 1 2 3 4 5".split(" ");
 var X = 0;
-var Y =0;
-var A = 0;
-var B = 0;
-var C = 0;
-var D = 0;
-var E = 0;
-var F = 0;
+var Y = 0;
+var A = split[0];
+//for testing
+//console.log(A);
+var B = split[1];
+//console.log(B);
+var C = split[2];
+//console.log(C);
+var D = split[3];
+//console.log(D);
+var E = split[4];
+//console.log(E);
+var F = split[5];
+//console.log(F);
 //Introduction / explanation
 console.log("I am a calculation device for solving equations similar to: ax + by = c. All you have to do is give me six numbers for to equations were X and Y equal the same thing.");
 //prompt for numbers
 //save numbers to variables
+var prompt = require('prompt')
 
+
+var schema = {
+    properties: {
+      Numbers: {
+        pattern: /^[0-9](\s)+$/,
+        message: 'Separate each number with a space.',
+        required: true
+      }
+    }
+  };
+
+
+prompt.start();
+
+prompt.get(schema, function (err, result) {
+	if (err) { return onErr(err); }
+	console.log('  Numbers: ' + result.Numbers);
+	result.Numbers = split;
 //calculate answer
+
 X = ((C * E) - (F * B)) / ((A * E) - (D * B));
 Y = ((A * F) - (D * F)) / ((A * C) - (D * B));
 //display correct answer
@@ -21,4 +54,5 @@ console.log(A + "x + " + B + "y = " + C);
 console.log(" and, ");
 console.log(D + "x + " + E + "y = " + F);
 console.log("In this equation, 'X' is equal to " + X + ", and 'Y' is equal to " + Y + ".");
+});
 //All done!
