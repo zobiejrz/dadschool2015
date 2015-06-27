cd42a0c16d9a72121591611b6cadee67ee56a1a8
zobiejrz <ben@zobie.com>
2015-06-27 13:04:11 -0400

    homework03.js one more to go
    The program is now fully functional, I will just be making it so it
    outputs 'X' and 'Y' to only 3 decimal places

--- homework03.js
+++ homework03.js
@@ -1,25 +1,18 @@
-function onErr(err)
-{
-	console.log(err);
-	return 1;
+function onErr(err) {
+  console.log(err);
+  return 1;
 }
 //initialize variables
-var split = "1 2 3 4 5 6".split(" ");
-var X = 0;
-var Y = 0;
-var A = split[0];
-//for testing
-//console.log(A);
-var B = split[1];
-//console.log(B);
-var C = split[2];
-//console.log(C);
-var D = split[3];
-//console.log(D);
-var E = split[4];
-//console.log(E);
-var F = split[5];
-//console.log(F);
+
+var x = 0,
+    y = 0;
+var a = 0,
+    b = 0,
+    c = 0,
+    d = 0,
+    e = 0,
+    f = 0;
+
 //Introduction / explanation
 console.log("I am a calculation device for solving equations similar to: ax + by = c. All you have to do is give me six numbers for to equations were X and Y equal the same thing.");
 //prompt for numbers
@@ -28,31 +21,47 @@ var prompt = require('prompt')
 
 
 var schema = {
-    properties: {
-      Numbers: {
-        pattern: /([1-9])\s/,
-        message: 'Separate each number with a space.',
-        required: true
-      }
+  properties: {
+    Numbers: {
+      pattern: /([1-9])\s/,
+      message: 'Separate each number with a space.',
+      required: true
     }
-  };
+  }
+};
 
 
 prompt.start();
 
 prompt.get(schema, function (err, result) {
-	if (err) { return onErr(err); }
-	console.log('  Numbers: ' + result.Numbers);
-	result.Numbers = split;
-//calculate answer
+  if (err) { return onErr(err); }
+  console.log('  Numbers: ' + result.Numbers);
 
-X = ((C * E) - (F * B)) / ((A * E) - (D * B));
-Y = ((A * F) - (D * F)) / ((A * C) - (D * B));
-//display correct answer
-console.log("Using the number you gave us, the equation is:");
-console.log(A + "x + " + B + "y = " + C);
-console.log(" and, ");
-console.log(D + "x + " + E + "y = " + F);
-console.log("In this equation, 'X' is equal to " + X + ", and 'Y' is equal to " + Y + ".");
+  var splitNumbers = result.Numbers.split (" ");
+  a = splitNumbers[0];
+  console.log ("\ta = " + a);
+  b = splitNumbers[1];
+  console.log ("\tb = " + b);
+  c = splitNumbers[2];
+  console.log ("\tc = " + c);
+  d = splitNumbers[3];
+  console.log ("\td = " + d);
+  e = splitNumbers[4];
+  console.log ("\te = " + e);
+  f = splitNumbers[5];
+  console.log ("\tf = " + f);
+  
+
+  //calculate answer
+
+
+  x = ((c * e) - (f * b)) / ((a * e) - (d * b));
+  y = ((a * f) - (d * c)) / ((a * e) - (d * b));
+  //display correct answer
+  console.log("Using the number you gave us, the equation is:");
+  console.log(a + "x + " + b + "y = " + c);
+  console.log(" and, ");
+  console.log(d + "x + " + e + "y = " + f);
+  console.log("In this equation, 'X' is equal to " + x + ", and 'Y' is equal to " + y + ".");
 });
 //All done!
