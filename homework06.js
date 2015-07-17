53b02a8c09fae55126cb1e01cd265be816755766
zobiejrz <ben@zobie.com>
2015-07-17 16:20:58 -0400

    homework06.js

---
+++ homework06.js
@@ -0,0 +1,19 @@
+// prompt for input
+// input permitted (R / N Number)
+
+
+// if credits greater than 12
+  //You are ____ registering for _____ credits.
+    //you are a full-timer
+    //fee = ____
+
+// if credits are less than 0
+  // You are ____ registering for _____ credits.
+    // try again later
+    
+// if credits are less than 10
+  //You are ____ registering for _____ credits.
+    //you are a part-timer
+    //fee = ____
+
+//terminate
