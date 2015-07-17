d8e26adee0c6414c7b497892ae5ae7c8555f0d70
zobiejrz <ben@zobie.com>
2015-07-17 10:56:56 -0400

    starting homework05.js

---
+++ homework05.js
@@ -0,0 +1,23 @@
+
+
+function main()
+{
+	//initalize variables
+	var typeA = 0;
+	var typeB = 0;
+	var typeC = 0;
+	
+	//introduction
+	
+	//prompt for var typeA B and C.
+	
+	//save prompt to variables
+	
+	//calculate output
+	
+	//console.log what typeA B and C are.
+	//console.log answer
+	
+	//terminate
+}
+main();
