6e81cf32fe748412a1331d86fc3e5e95706c635b
zobiejrz <ben@zobie.com>
2015-07-19 08:28:08 -0400

    homework01.js converting to node.js completed
    The program has been 100% converted to node.js

--- homework01.js
+++ homework01.js
@@ -1,44 +1,29 @@
-function isInt(n)
-{
-	var retVal = (n % 1 === 0);
- 	return retVal;
-}
+console.log("I am a personel calculation device for calculating perimeter and area of a square.");
+console.log("All you have to do is give my the length of a side.");
+var prompt = require('prompt')
 
-// return true if 'n' is a valid value. otherwise return false
-function isValidValue (n)
-{
-	if (isInt(n) && (n >= 0))
-	{
-		return true;
-	}
-	return false;
-}
 
-// print instruction
-alert("Hello, I am a personal calculation device for calculating the area and perimeter of a square, given the side length you give me.");
+var schema = {
+    properties: {
+      Side: {
+        pattern: /^[1-9]+$/,
+        message: 'Only positive integers are allowed.',
+        required: true
+      }
+    }
+  };
 
-// initialize variables
-var squareSide = 0;
-var perimeter = 0;
-var area = 0;
 
-// check if squareSide is valid
-do 
-{
-	// prompt for the length of the side of a square
-	// read and store squareSide
-	var promptUser = "What do you want to be the side length of the square?";
-	squareSide = prompt(promptUser);
-}
-while (!isValidValue(squareSide));
+prompt.start();
 
-// Calculate perimeter of the square
-// Calculate area of the square
-perimeter = squareSide * 4;
-area = squareSide * squareSide;
-
-// Print the perimeter of the square
-// Print the area of the square
-alert("The perimeter of a square with the side length of " + squareSide + " is " + perimeter + ". The area of a square with the same side length is " + area +".");
-
-// All done!
+prompt.get(schema, function (err, result) {
+	if (err) { return onErr(err); }
+	console.log('  Side: ' + result.Side);
+	var sideLength = result.Side;
+	
+	var perimeter = sideLength * 4;
+	var area = sideLength * sideLength;
+	
+	console.log("The area of this square is " + area);
+	console.log("The perimeter of this square is " + perimeter);
+});
