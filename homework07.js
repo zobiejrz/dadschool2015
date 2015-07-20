be05dcbe047c8bddbb08d5a0e635b1c4fdb1684b
zobiejrz <ben@zobie.com>
2015-07-20 08:57:41 -0400

    homework07.js adding the data
    started putting in the code to read the data from homework07.data

--- homework07.js
+++ homework07.js
@@ -4,6 +4,28 @@ function onErr(err) {
 }
 
 function main() {
+
+  var __dirname = "C:\\Users\\ben\\src\\dadschool2015";
+
+  var fs = require('fs');
+  var path = require('path');
+
+  var filePath = path.join(__dirname, 'homework07.data');
+
+  fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+    if (!err) {
+      console.log('>>>\n' + data + "\n<<<");
+      // response.writeHead(200, { 'Content-Type': 'text/html' });
+      // response.write(data);
+      // response.end();
+    } else {
+      console.log(err);
+    }
+
+  });
+  
+  
+  /*
   var resultNum = 0;
 
   console.log("I will calculate an approximation of pi if you give me what digit to approximate to.")
@@ -14,7 +36,7 @@ function main() {
     properties: {
       Digit: {
         //regex pattern that only allows positive integers
-        pattern: /^[1-9]\d*/,
+        pattern: /^[1-9]\d*TODO /,
         message: 'Error,You can only input a postitive integer',
         required: true
       }
@@ -35,6 +57,7 @@ function main() {
     console.log("Approximating pi to " + digits + " digits is approximatly");
     console.log("\t" + resultNum.toFixed(6));
   });
+  */
 }
 main();
 //terminate
