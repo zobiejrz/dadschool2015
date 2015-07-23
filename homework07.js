84484cf66f81a09c9d1da57530cffb355b85d70f
zobiejrz <ben@zobie.com>
2015-07-23 09:27:03 -0400

    Refactoring to consolidate math
    Refactored the code so that runAsBatch and runWithPrompt use the same
    code to run. Both can now be called in the main() function

--- homework07.js
+++ homework07.js
@@ -3,9 +3,60 @@ function onErr(err) {
   return 1;
 }
 
-function main() {
+/// This method will approximate PI based on the formula provided
+/// in the assignment and based on the provided precision.
+function calculatePi (precision)
+{
+  
+  if (precision <= 0)
+  {
+    onErr ("I cannnot calculate pi using that number (" + precision + ")");
+    return -1;
+  }
+  
+  var resultNum = 0;
+  for (var i = precision; i > 0; i--) {
+    resultNum = Math.pow(resultNum + (1 / i), 2);
+  }
+  resultNum = Math.sqrt(resultNum * 6);
+  return resultNum;
+}
 
-  var __dirname = "C:\\Users\\ben\\src\\dadschool2015";
+function runWithPrompt ()
+{
+    console.log("I will calculate an approximation of pi if you give me what digit to approximate to.");
+    // prompt for what digit of pi we go to
+
+    var prompt = require('prompt');
+
+    var schema = {
+      properties: {
+        Digit: {
+          //regex pattern that only allows positive integers
+          pattern: /^[1-9]\d?/,
+          message: 'Error,You can only input a postitive integer',
+          required: true
+        }
+      }
+    };
+
+    prompt.start();
+
+    prompt.get(schema, function (err, result) {
+      if (err) { return onErr(err); }
+      
+      var digits = result.Digit;
+      var pi = calculatePi(digits);
+      console.log("Approximating pi to " + digits + " digits is approximatly");
+      console.log("\t" + pi.toFixed(6));
+
+    });
+
+}
+
+function runAsBatch ()
+{
+   var __dirname = "C:\\Users\\ben\\src\\dadschool2015";
 
   var fs = require('fs');
   var path = require('path');
@@ -24,61 +75,25 @@ function main() {
     console.log("I will be calculating >" + numElements + "< different entries from " + filePath);
 
     for (var i = 1; i <= numElements; i++) {
-      var dat = inputData[i];
-      if (dat > 0) {
-         for (var j = 1; j < dat; j++) {
-          var result = 0;
-          result = result + Math.pow(1 / j, 2);
-         }
-        result = Math.sqrt(result * 6);
 
-        console.log("Approximating pi to " + dat + " digits is approximatly");
-        console.log("\t" + result.toFixed(6));
-      }
-      else {
-        console.log("I cannnot calculate pi using that number (" + dat + ")");
-      }
+      var digits = inputData[i];
+      var pi = calculatePi(digits);
+      console.log("Approximating pi to " + digits + " digits is approximatly");
+      console.log("\t" + pi.toFixed(6));
+
     }
-
-
-    //The code from this point on will run the prompt version
-    
-    // var resultNum = 0;
-    // var counter = 1;
-    // console.log("I will calculate an approximation of pi if you give me what digit to approximate to.");
-    //prompt for what digit of pi we go to
-
-    // var prompt = require('prompt');
-
-    // var schema = {
-    //   properties: {
-    //     Digit: {
-    //       //regex pattern that only allows positive integers
-    //       pattern: /^[1-9]\d/,
-    //       message: 'Error,You can only input a postitive integer',
-    //       required: true
-    //     }
-    //   }
-    // };
-
-    // prompt.start();
-
-    // prompt.get(schema, function (err, result) {
-    //   if (err) { return onErr(err); }
-    //   result.Digit = dataSplit[counter];
-    //   var digits = result.Digit;
-
-    //   //for- loop that will actually calculate what the app. of pi is
-    //   for (var i = digits; i > 0; i--) {
-    //     resultNum = Math.pow(resultNum + (1 / i), 2);
-    //   }
-    //   resultNum = Math.sqrt(resultNum * 6);
-    //   console.log("Approximating pi to " + digits + " digits is approximatly");
-    //   console.log("\t" + resultNum.toFixed(6));
-
-    // });
 
   });
 }
+
+function main()
+{
+
+  runWithPrompt();
+  // runAsBatch();
+ 
+}
+
+// Execute the app!
 main();
-//terminate
+// Terminate!
