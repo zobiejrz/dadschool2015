7eb60e882ec1da71f1a091ac3f9863d33c6dc61d
zobiejrz <ben@zobie.com>
2015-07-23 08:41:56 -0400

    homework07.js batch version complete
    The specs said the program had to run the code using the data from
    homework07.data, so here it is

--- homework07.js
+++ homework07.js
@@ -15,49 +15,70 @@ function main() {
   fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
     if (!err) {
       console.log('>>>\n' + data + "\n<<<");
-      // response.writeHead(200, { 'Content-Type': 'text/html' });
-      // response.write(data);
-      // response.end();
-    } else {
+    }
+    else {
       console.log(err);
     }
+    var inputData = data.split(/\s+/);
+    var numElements = inputData[0]; // the specs say that the first element is always the number of elements that will be processed
+    console.log("I will be calculating >" + numElements + "< different entries from " + filePath);
 
-  });
-  
-  
-  /*
-  var resultNum = 0;
+    for (var i = 1; i <= numElements; i++) {
+      var dat = inputData[i];
+      if (dat > 0) {
+         for (var j = 1; j < dat; j++) {
+          var result = 0;
+          result = result + Math.pow(1 / j, 2);
+         }
+        result = Math.sqrt(result * 6);
 
-  console.log("I will calculate an approximation of pi if you give me what digit to approximate to.")
-  //prompt for what digit of pi we go to
-  var prompt = require('prompt');
-
-  var schema = {
-    properties: {
-      Digit: {
-        //regex pattern that only allows positive integers
-        pattern: /^[1-9]\d*TODO /,
-        message: 'Error,You can only input a postitive integer',
-        required: true
+        console.log("Approximating pi to " + dat + " digits is approximatly");
+        console.log("\t" + result.toFixed(6));
+      }
+      else {
+        console.log("I cannnot calculate pi using that number (" + dat + ")");
       }
     }
-  };
 
-  prompt.start();
 
-  prompt.get(schema, function (err, result) {
-    if (err) { return onErr(err); }
-    var digits = result.Digit;
+    //The code from this point on will run the prompt version
     
-    //for- loop that will actually calculate what the app. of pi is
-    for(var i = digits; i > 0; i--) {
-      resultNum = Math.pow(resultNum + (1 / i), 2);
-    }
-    resultNum = Math.sqrt(resultNum * 6);
-    console.log("Approximating pi to " + digits + " digits is approximatly");
-    console.log("\t" + resultNum.toFixed(6));
+    // var resultNum = 0;
+    // var counter = 1;
+    // console.log("I will calculate an approximation of pi if you give me what digit to approximate to.");
+    //prompt for what digit of pi we go to
+
+    // var prompt = require('prompt');
+
+    // var schema = {
+    //   properties: {
+    //     Digit: {
+    //       //regex pattern that only allows positive integers
+    //       pattern: /^[1-9]\d/,
+    //       message: 'Error,You can only input a postitive integer',
+    //       required: true
+    //     }
+    //   }
+    // };
+
+    // prompt.start();
+
+    // prompt.get(schema, function (err, result) {
+    //   if (err) { return onErr(err); }
+    //   result.Digit = dataSplit[counter];
+    //   var digits = result.Digit;
+
+    //   //for- loop that will actually calculate what the app. of pi is
+    //   for (var i = digits; i > 0; i--) {
+    //     resultNum = Math.pow(resultNum + (1 / i), 2);
+    //   }
+    //   resultNum = Math.sqrt(resultNum * 6);
+    //   console.log("Approximating pi to " + digits + " digits is approximatly");
+    //   console.log("\t" + resultNum.toFixed(6));
+
+    // });
+
   });
-  */
 }
 main();
 //terminate
