c88a632ae98e9d7aadbb01175298f049e1cee373
zobiejrz <ben@zobie.com>
2015-08-01 08:16:48 -0400

    Correctly downloads data, and correctly splits the data
    Correctly does everything that is written. Still need to finish writing
    the code though :-)

--- homework09.js
+++ homework09.js
@@ -1,18 +1,47 @@
-// Function that will calculate the LCM and the GCF
+function onErr(err)//On Error function for when there is an error
+{
+  console.log(err);
+  return 1;
+}
+function calculate(fileName)// Function that will calculate the LCM and the GCF
+{
+  // Download data from
+  // Users\ben\src\dadschool2015\homework09.data
+  var __dirname = "C:\\Users\\ben\\src\\dadschool2015";
 
-// Download data from
-// Users\ben\src\dadschool2015\homework09.data
+  var fs = require('fs');
+  var path = require('path');
 
-// Run loop to loop through the data lines
-// Run loop to calculate the LCM
-// Run loop to calculate the GCF
+  var filePath = path.join(__dirname, fileName);
 
-// Console.log the two data numbers, and their LCM and GCF
-// End data loop
+  fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {// Start of file download code
+    if (!err) {
+      console.log("Now downloading data from " + filePath);
+      console.log('>>>\n' + data + "\n<<<");
+    }
+    else {
+      console.log(err);
+    }
+    var inputData = data.split(" ");
+    console.log("Input Data:\n" + inputData);
+    // for (var i = inputData.length; i > 0; i--) // Run loop to loop through the data lines
+    // {
+    //   var digitA = inputData[i];
+    //   var digitB = inputData[i + 1];
+    //   // Run loop to calculate the LCM
+    //   while(digitA != digitB){
+        
+    //   }
+      
+      // Run loop to calculate the GCF
 
-// End calculate function
+      // Console.log the two data numbers, and their LCM and GCF
+    // }// End data loop
+  });//End of file download code
+} // End calculate function
 
-// Main function
-// Call the calculate function
-
-// End main function
+function main()// Main function
+{
+calculate('homework09.data');// Call the calculate function
+}// End main function
+main();
