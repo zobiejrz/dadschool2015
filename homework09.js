6a2a3935443385802f67929aa19f4cec766d6bcf
zobiejrz <ben@zobie.com>
2015-08-01 07:59:53 -0400

    Writing the Framework
    Wrote down the initial framework of the program.

---
+++ homework09.js
@@ -0,0 +1,18 @@
+// Function that will calculate the LCM and the GCF
+
+// Download data from
+// Users\ben\src\dadschool2015\homework09.data
+
+// Run loop to loop through the data lines
+// Run loop to calculate the LCM
+// Run loop to calculate the GCF
+
+// Console.log the two data numbers, and their LCM and GCF
+// End data loop
+
+// End calculate function
+
+// Main function
+// Call the calculate function
+
+// End main function
