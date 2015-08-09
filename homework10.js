398539215cb1a5cf10cb293f3ecb86fa818c5438
zobiejrz <ben@zobie.com>
2015-08-09 18:04:12 -0400

    Finished the initial algorithm in numToWord()
    The algorithm will output an understandable string, but the -teen
    numbers are not in place yet.
    Example:
    input: 17
    output: 'ten seven'
    intended output: 'seventeen'
    Also, there are sloppy blank spaces for the unused numbers.
    Example:
    input: 17
    output: '  ten seven'
    intended output: 'seventeen'

--- homework10.js
+++ homework10.js
@@ -2,31 +2,107 @@ function onErr(err) {
   console.log(err);
   return 1;
 }
-function processNum (num, count){
-  for( var i = 0; i < count; i++ ){
-    if( num == 9 ) { 
-      console.log("Nine");
-    } else if ( num == 8 ) {
-      console.log("Eight");
-    } else if ( num == 7 ) {
-      console.log("Seven");
-    } else if ( num == 6 ) {
-      console.log("Six");
-    } else if ( num == 5 ) {
-      console.log("Five");
-    } else if ( num == 4 ) {
-      console.log("Four");
-    } else if ( num == 3 ) {
-      console.log("Three");
-    } else if ( num == 2 ) {
-      console.log("Two");
-    } else if ( num == 1 ){
-      console.log("One");
+function numToWord(intVal) {
+  var place = intVal.length;
+  var placeOne = '';
+  var placeTwo = '';
+  var placeThree = '';
+  var placeFour = '';
+  var placeHolder = intVal.split("");
+  for (var k = 0; k < placeHolder.length; k++ ){
+
+    if (place == 4) {
+      if (placeHolder[k] == 9) {
+        placeOne = 'nine thousand';
+      } else if (placeHolder[k] == 8) {
+        placeOne = 'eight thousand';
+      } else if (placeHolder[k] == 7) {
+        placeOne = 'seven thousand';
+      } else if (placeHolder[k] == 6) {
+        placeOne = 'six thousand';
+      } else if (placeHolder[k] == 5) {
+        placeOne = 'five thousand';
+      } else if (placeHolder[k] == 4) {
+        placeOne = 'four thousand';
+      } else if (placeHolder[k] == 3) {
+        placeOne = 'three thousand';
+      } else if (placeHolder[k] == 2) {
+        placeOne = 'two thousand';
+      } else if (placeHolder[k] == 1) {
+        placeOne = 'one thousand';
+      }
+    } else if (place == 3) {
+      if (placeHolder[k] == 9) {
+        placeTwo = 'nine hundred';
+      } else if (placeHolder[k] == 8) {
+        placeTwo = 'eight hundred';
+      } else if (placeHolder[k] == 7) {
+        placeTwo = 'seven hundred';
+      } else if (placeHolder[k] == 6) {
+        placeTwo = 'six hundred';
+      } else if (placeHolder[k] == 5) {
+        placeTwo = 'five hundred';
+      } else if (placeHolder[k] == 4) {
+        placeTwo = 'four hundred';
+      } else if (placeHolder[k] == 3) {
+        placeTwo = 'three hundred';
+      } else if (placeHolder[k] == 2) {
+        placeTwo = 'two hundred';
+      } else if (placeHolder[k] == 1) {
+        placeTwo = 'one hundred';
+      }
+    } else if (place == 2) {
+      if (placeHolder[k] == 9) {
+        placeThree = 'ninety';
+      } else if (placeHolder[k] == 8) {
+        placeThree = 'eighty';
+      } else if (placeHolder[k] == 7) {
+        placeThree = 'seventy';
+      } else if (placeHolder[k] == 6) {
+        placeThree = 'sixty';
+      } else if (placeHolder[k] == 5) {
+        placeThree = 'fifty';
+      } else if (placeHolder[k] == 4) {
+        placeThree = 'fourty';
+      } else if (placeHolder[k] == 3) {
+        placeThree = 'thirty';  
+      } else if (placeHolder[k] == 2) {
+        placeThree = 'twenty';
+      } else if (placeHolder[k] == 1) {
+        placeThree = 'ten';
+      }
+    } else if (place == 1) {
+      if (placeHolder[k] == 9) {
+        placeFour = 'nine';
+      } else if (placeHolder[k] == 8) {
+        placeFour = 'eight';
+      } else if (placeHolder[k] == 7) {
+        placeFour = 'seven';
+      } else if (placeHolder[k] == 6) {
+        placeFour = 'six';
+      } else if (placeHolder[k] == 5) {
+        placeFour = 'five';
+      } else if (placeHolder[k] == 4) {
+        placeFour = 'four';
+      } else if (placeHolder[k] == 3) {
+        placeFour = 'three';
+      } else if (placeHolder[k] == 2) {
+        placeFour = 'two';
+      } else if (placeHolder[k] == 1) {
+        placeFour = 'one';
+      }
+
     }
+
+      place--;
   }
+    console.log( 'Output: \n>>>' + placeOne + ' ' + placeTwo + ' ' + placeThree + ' ' + placeFour + '\n<<<');
 }
-function programRun(fileName) { // Function that will input a number and output the same number as a string
-  // Download [generic].data
+
+function main() {
+  var fileName = 'homework10.data'; // Process homework10.data into a string
+  
+   // Download [generic].data
   // Split [generic].data
   var __dirname = "C:\\Users\\ben\\src\\dadschool2015";
 
@@ -43,29 +119,15 @@ function programRun(fileName) { // Function that will input a number and output 
     else {
       console.log(err);
     }
-    var num = data.split(/\s+/);
+    var dataNumber = data.split(/\s+/);
     // Loop through [generic].data
-    for ( var i = 0; i < num.length; i++ ){
-      var currentNum = num[i].split("");
-    // Process [generic].data into a string
-    // Display number as string
-      console.log("currentNum: " + currentNum);
-      if ( currentNum.length == 4 ) {
-        processNum ( currentNum, 4 );
-      } else if ( currentNum.length == 3 ) {
-        processNum ( currentNum, 3 );
-      } else if ( currentNum.length == 2 ) {
-        processNum ( currentNum, 2 );
-      } else if ( currentNum.length == 1 ){
-        processNum ( currentNum, 1);
-      } else {
-        console.log (currentNum + " is an incorrect input. Only inputs that are greater than 0 and less than 10000 can be processed.")
-      }
-  }
-}); // End of file download code
-}
-function main() {
-  programRun('homework10.data'); // Process homework10.data into a string
+    for (var i = 0; i < dataNumber.length; i++) {
+      var currentNum = dataNumber[i];
+      numToWord(currentNum);
+    }
+  }); // End of file download code
+  
 }
 main(); // Run program
+
 // The program has finished!
